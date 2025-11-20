0b3afebfec98dcf23bf4e7752e64a18d7506e511
Aidil Gonzales <1008320231@unajma.edu.pe>
2025-11-20 16:08:43 -0500

    Proyecto inicial con Angular y Firebase

---
+++ src/app/app.config.ts
@@ -0,0 +1,15 @@
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes), provideFirebaseApp(() => initializeApp({ projectId: "gestor-tareas-aidil", appId: "1:620061689340:web:7c21beec052a60d922de79", storageBucket: "gestor-tareas-aidil.firebasestorage.app", apiKey: "AIzaSyCqlKU31D8ZNXjPzKhL859YjqzApD9lGyc", authDomain: "gestor-tareas-aidil.firebaseapp.com", messagingSenderId: "620061689340", projectNumber: "620061689340", version: "2" })), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())
+  ]
+};
